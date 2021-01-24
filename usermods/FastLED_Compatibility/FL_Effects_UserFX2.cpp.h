282b58af7e011d0e6509ba9bf5e3f180bdd10b0d
Louis Beaudoin <louis@embedded-creations.com>
2021-01-24 23:49:46 +0000

    Add support for FastLED triggers (FL_EVERY_N_MILLIS, etc), FL_FUNCTIONS to nest local functions inside mode - Support functions FL_SETUP() and Add FL_ALLOC without array - Patterns to test FL_EVERY_N_MILLIS and FL_FUNCTIONS

--- usermods/FastLED_Compatibility/FL_Effects_UserFX2.cpp.h
+++ usermods/FastLED_Compatibility/FL_Effects_UserFX2.cpp.h
@@ -55,7 +55,6 @@ uint16_t WS2812FX::mode_ScrollingWash(void) {
   return FRAMETIME;
 }
 
-
 // Fire2012 by Mark Kriegsman. Converted to WLED by Andrew Tuline. 
 // original https://github.com/FastLED/FastLED/blob/master/examples/Fire2012/Fire2012.ino
 uint16_t WS2812FX::mode_2Dfire2012(void) {
@@ -101,6 +100,157 @@ uint16_t WS2812FX::mode_2Dfire2012(void) {
   // TODO: we might not need to update leds every time based on millis check?
 
   END_FASTLED_XY_COMPATIBILITY();
+
+  return FRAMETIME;
+}
+
+// https://github.com/marmilicious/FastLED_examples/blob/master/every_n_timer_variable.ino
+uint16_t WS2812FX::mode_blinktriggers(void) {
+  FL_ALLOC(
+    boolean counterTriggered = 0;  // Event triggered? [1=true, 0=false]
+    CEveryNMilliseconds triggerTimer;
+    CEveryNMillis flMillisTrigger1;
+    CEveryNSeconds timingObj;
+  );
+
+  int msUntilNextPass = FRAMETIME;
+
+  const uint16_t timerA = 3000;  // How often to run Event A [milliseconds]
+  const uint16_t timerB = 500;  // How long after A to run Event B [milliseconds]
+
+  // This runs once when the effect starts, similar to setup() in a FastLED sketch
+  FL_SETUP() {
+    FL_STATICVAR(counterTriggered) = 0;
+
+    // make a macro?  These calls replace static CEveryNMilliseconds triggerTimer(timerB);
+    FL_STATICVAR(triggerTimer).reset();
+    FL_STATICVAR(triggerTimer).setPeriod(timerB);
+  }
+
+  FL_EVERY_N_MILLIS(flMillisTrigger1, timerA) {
+    // do Event A stuff
+    fill_solid(leds, NUM_LEDS, CHSV(random8(),255,128));
+    FL_STATICVAR(counterTriggered) = 1;  // Set to True
+    FL_STATICVAR(triggerTimer).reset();  // Start trigger timer
+  }
+
+  if (FL_STATICVAR(counterTriggered) == 1) {  // Will only be True if Event A has started
+    if (FL_STATICVAR(triggerTimer)) {  // Check if triggerTimer time reached
+      // do Event B stuff
+      for (uint8_t i=0; i<NUM_LEDS/2; i++){
+        leds[random16(NUM_LEDS)] = CRGB::Red;
+      }
+      FL_STATICVAR(counterTriggered) = 0;  // Set back to False
+    }
+  }
+
+#if 0
+  // this block doesn't translate easily to WLED as it uses delays in a loop and nested FastLED.show() calls
+  FL_EVERY_N_SECONDS(timingObj, 20) {
+    // This initally defaults to 20 seconds, but then will change the run
+    // period to a new random number of seconds from 10 and 30 seconds.
+    // You can name "timingObj" whatever you want.
+    FL_STATICVAR(timingObj).setPeriod( random8(10,31) );
+    fill_solid(leds, NUM_LEDS, CRGB::Black);
+    for (uint16_t i=0; i<NUM_LEDS*3; i++){
+      leds[random8(NUM_LEDS)] = CRGB::Black;
+      leds[random8(NUM_LEDS)] = CHSV(random8(), random8(140,255), random8(50,255));
+      FastLED.show();
+      delay(random8(20,80));
+    }
+    msUntilNextPass = 500
+  }
+#endif
+
+  END_FASTLED_COMPATIBILITY();
+
+  return msUntilNextPass;  
+}
+
+// https://gist.github.com/kriegsman/5408ecd397744ba0393e
+uint16_t WS2812FX::mode_fl_twinkles(void) {
+  FL_ALLOC_WITH_1_ARRAY_XY(
+
+    , uint8_t, directionFlags, (NUM_LEDS+7) / 8);
+
+  enum { GETTING_DARKER = 0, GETTING_BRIGHTER = 1 };
+
+  const int STARTING_BRIGHTNESS = 64;
+  const int FADE_IN_SPEED       = 32;
+  const int FADE_OUT_SPEED      = 20;
+  const int DENSITY            = 255;
+
+  FL_FUNCTIONS_BEGIN // vvvvvvvvv Local Functions go here
+    void brightenOrDarkenEachPixel( fract8 fadeUpAmount, fract8 fadeDownAmount)
+    {
+     for( uint16_t i = 0; i < NUM_LEDS; i++) {
+        if( getPixelDirection(i) == GETTING_DARKER) {
+          // This pixel is getting darker
+          leds[i] = makeDarker( leds[i], fadeDownAmount);
+        } else {
+          // This pixel is getting brighter
+          leds[i] = makeBrighter( leds[i], fadeUpAmount);
+          // now check to see if we've maxxed out the brightness
+          if( leds[i].r == 255 || leds[i].g == 255 || leds[i].b == 255) {
+            // if so, turn around and start getting darker
+            setPixelDirection(i, GETTING_DARKER);
+          }
+        }
+      }
+    }
+
+    CRGB makeBrighter( const CRGB& color, fract8 howMuchBrighter) 
+    {
+      CRGB incrementalColor = color;
+      incrementalColor.nscale8( howMuchBrighter);
+      return color + incrementalColor;
+    }
+
+    CRGB makeDarker( const CRGB& color, fract8 howMuchDarker) 
+    {
+      CRGB newcolor = color;
+      newcolor.nscale8( 255 - howMuchDarker);
+      return newcolor;
+    }
+
+    bool getPixelDirection( uint16_t i) {
+      uint16_t index = i / 8;
+      uint8_t  bitNum = i & 0x07;
+      // using Arduino 'bitRead' function; expanded code below
+      return bitRead( FL_STATICVAR(directionFlags)[index], bitNum);
+      // uint8_t  andMask = 1 << bitNum;
+      // return (directionFlags[index] & andMask) != 0;
+    }
+
+    void setPixelDirection( uint16_t i, bool dir) {
+      uint16_t index = i / 8;
+      uint8_t  bitNum = i & 0x07;
+      // using Arduino 'bitWrite' function; expanded code below
+      bitWrite( FL_STATICVAR(directionFlags)[index], bitNum, dir);
+      //  uint8_t  orMask = 1 << bitNum;
+      //  uint8_t andMask = 255 - orMask;
+      //  uint8_t value = directionFlags[index] & andMask;
+      //  if( dir ) {
+      //    value += orMask;
+      //  }
+      //  directionFlags[index] = value;
+    }
+  FL_FUNCTIONS_END // ^^^^^^^^^ Local Functions go here
+
+  // Make each pixel brighter or darker, depending on
+  // its 'direction' flag.
+  FL_FUNCTION(brightenOrDarkenEachPixel)( FADE_IN_SPEED, FADE_OUT_SPEED);
+  
+  // Now consider adding a new random twinkle
+  if( random8() < DENSITY ) {
+    int pos = random16(NUM_LEDS);
+    if( !leds[pos]) {
+      leds[pos] = ColorFromPalette( currentPalette, random8(), STARTING_BRIGHTNESS, NOBLEND);
+      FL_FUNCTION(setPixelDirection)(pos, GETTING_BRIGHTER);
+    }
+  }
+
+  END_FASTLED_COMPATIBILITY();
 
   return FRAMETIME;
 }
--- usermods/FastLED_Compatibility/FL_Wrapper_UserFX1.cpp.h
+++ usermods/FastLED_Compatibility/FL_Wrapper_UserFX1.cpp.h
@@ -67,7 +67,7 @@ void * flStructPtr;
   setKMatrixWidth(_segmentmaps[_segment_index].matrixWidth);                                            \
   setKMatrixHeight(_segmentmaps[_segment_index].matrixHeight);                                          \
   NUM_LEDS = (kMatrixWidth*kMatrixHeight);                                                              \
-  struct fl_Struct{ FL_STRUCT_CONTENTS };                                                               \
+  struct fl_Struct{ FL_STRUCT_CONTENTS TYPE1 * NAME1; };                                                               \
   int flAllocSize = (sizeof(CRGB) * NUM_LEDS) + sizeof(fl_Struct) + (sizeof(TYPE1) * NUM_ELEMENTS1);    \
   if(!SEGENV.allocateData(flAllocSize)) return mode_static();                                           \
   uint8_t * segmentDataPtr = SEGENV.data;                                                               \
@@ -76,6 +76,17 @@ void * flStructPtr;
   flStructPtr = segmentDataPtr;                                                                         \
   segmentDataPtr += sizeof(fl_Struct);                                                                  \
   TYPE1 * NAME1 = (TYPE1 *)(segmentDataPtr);                                                            \
+  ((fl_Struct*)flStructPtr)->NAME1 = NAME1;                                                             \
+
+#define FL_ALLOC(FL_STRUCT_CONTENTS)                                  \
+  NUM_LEDS = SEGLEN;                                                  \
+  struct fl_Struct{ FL_STRUCT_CONTENTS };                             \
+  int flAllocSize = (sizeof(CRGB) * NUM_LEDS) + sizeof(fl_Struct);    \
+  if(!SEGENV.allocateData(flAllocSize)) return mode_static();         \
+  uint8_t * segmentDataPtr = SEGENV.data;                             \
+  leds = reinterpret_cast<CRGB*>(segmentDataPtr);                     \
+  segmentDataPtr += (sizeof(CRGB) * NUM_LEDS);                        \
+  flStructPtr = segmentDataPtr;                                       \
 
 #define FL_STATICVAR(TESTVAR) (((fl_Struct*)flStructPtr)->TESTVAR)
 
@@ -97,3 +108,18 @@ void * flStructPtr;
 #define END_FASTLED_COMPATIBILITY()       \
           writeLedsArrayToWled(leds);     \
 
+#define FL_FUNCTIONS_BEGIN struct fl_Functions { 
+#define FL_FUNCTIONS_END  }; fl_Functions flFunctions;
+#define FL_FUNCTION(FUNCTION) flFunctions.FUNCTION 
+
+#define FL_EVERY_N(NAME,N) \
+  if(!SEGENV.call) {((fl_Struct*)flStructPtr)->NAME.reset(); ((fl_Struct*)flStructPtr)->NAME.setPeriod(N); } \
+  if(((fl_Struct*)flStructPtr)->NAME)
+
+#define FL_EVERY_N_MILLIS   FL_EVERY_N
+#define FL_EVERY_N_SECONDS  FL_EVERY_N
+#define FL_EVERY_N_BSECONDS FL_EVERY_N
+#define FL_EVERY_N_MINUTES  FL_EVERY_N
+#define FL_EVERY_N_HOURS    FL_EVERY_N
+
+#define FL_SETUP() if(!SEGENV.call)
